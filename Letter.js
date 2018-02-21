881b91867336d06729e89d483196ada668e38dc3
SpotHeroSarah <spotherosarah@SpotHeroAdmins-MacBook-Air-4.local>
2018-02-21 15:32:56 -0600

    added to Word.js

--- Letter.js
+++ Letter.js
@@ -2,23 +2,33 @@
 // an underlying character or a blank placeholder (such as an underscore), depending on whether 
 // or not the user has guessed the letter. That means the constructor should define:
 
+//Letter CONSTRUCTOR!!!!!
 function Letter(char) {
+    //stores the underlying character in Hangman game
     this.char = char;
+    //a boolean that tracks whether or not a letter has been guessed
     this.guessed = false;
+    //This function either returns the underlying char or an underscore
+    //With "toString" JavaScript will call that function automatically whenever casting that object to a string 
     this.toString = function() {
         if (this.guessed)
         {
-        	return this.char;
+    //If it was guessed, return the underlying character!
+            return this.char;
         }
+    //else return and underscore
         return "_"; 
     }
+    //Checks if a letter has been inputed and will mark the letter as guessed or not guessed
     this.isMatch = function(letter) {
         if (letter === this.char) {
             this.guessed = true;
         }
+        return false;
     }
 };
 
+//Exports for pick up later :)
 module.exports = Letter;
 
 //to test
--- Word.js
+++ Word.js
@@ -10,48 +10,64 @@
 // function defined in Letter.js) that displays the character or 
 // an underscore and concatenate those together.
 
+//first I need to access my Letter file and constructor
 var Letter = require('./Letter.js');
+//testing testing 123
 //console.log(Letter);
 
 input = "apple";
 
+//Word constructor
 function Word(input) {
+    //creating an array that is going to hold all the letters in the word
     var letArr = [];
-    for (i=0; i < input.length; i++) {
+    //whatever gets inputed as a word, take that and push each individ letter into the letArr
+    for (var letter in input) {
         
-        letArr.push(new Letter(input[i]));
+        letArr.push(new Letter(input[letter].toUpperCase()));
     }
     //console.log(letArr);
-    this.letArr = letArr;
-    this.numbersCorrect;
-    console.log(this.letArr);
+    //save the letArr into a constructor variable
+    this.word = letArr;
+    //console.log(this.word);
+    //save the number of guesses made correctly
+    this.numbersCorrect = 0;
+    //console.log(this.word);
+    //returns a string representing the input word
     this.showLetters = function(){
         var wordString = "";
-        for(var i = 0; i<this.letArr.length; i++)
+        for(var i = 0; i<this.word.length; i++)
         {
-                wordString += this.letArr[i] + " ";
+                wordString += this.word[i] + " ";
         }
         console.log(wordString);
     }
     // A function that takes a character as an argument and calls the 
     // guess function on each letter object (the second function 
     // defined in Letter.js)
-    // this.guessed = function(char) {
-    //     var correct = false;
-    //     for (var i = 0; this.letArr.length; i++) {
-    //         if(letArr[i].isMatch(char)) {
-    //             correct = true;
-    //             this.numbersCorrect++;
-    //         }
-    //         console.log(this.numbersCorrect);
-    //         console.log(correct);
-    //     }
-    // }
+    this.checkGuessed = function(char) {
+        var correct = false;
+        for (var i = 0; this.word.length; i++) {
+            if(this.word[i] === char) {
+                correct = true;
+                this.numbersCorrect++;
+            } else {
+                break;
+            }
+            console.log(this.numbersCorrect);
+            console.log(correct);
+        }
+        return correct;
+    }
 
-}
+};
 
+//Testing testing 123
 var word = new Word(input);
 word.showLetters();
+word.checkGuessed("A");
+word.showLetters();
+//console.log(word.checkGuessed("A"));
 //word.guessed("a");
 // if (A.isMatch("A")
 // console.log(A+" ");
