ab6e8d6622c3d3f4a74c7d6e6d7db1d4d936b9e0
sarleone <sarah.m.leone@gmail.com>
2018-02-23 09:33:27 -0600

    adding index work

--- Word.js
+++ Word.js
@@ -10,64 +10,49 @@
 // function defined in Letter.js) that displays the character or 
 // an underscore and concatenate those together.
 
-//first I need to access my Letter file and constructor
 var Letter = require('./Letter.js');
-//testing testing 123
 //console.log(Letter);
 
 input = "apple";
 
-//Word constructor
 function Word(input) {
-    //creating an array that is going to hold all the letters in the word
     var letArr = [];
-    //whatever gets inputed as a word, take that and push each individ letter into the letArr
-    for (var letter in input) {
+    for (i=0; i < input.length; i++) {
         
-        letArr.push(new Letter(input[letter].toUpperCase()));
+        letArr.push(new Letter(input[i]));
     }
     //console.log(letArr);
-    //save the letArr into a constructor variable
-    this.word = letArr;
-    //console.log(this.word);
-    //save the number of guesses made correctly
-    this.numbersCorrect = 0;
-    //console.log(this.word);
-    //returns a string representing the input word
+    this.letArr = letArr;
+    this.numbersCorrect;
+    console.log(this.letArr);
     this.showLetters = function(){
         var wordString = "";
-        for(var i = 0; i<this.word.length; i++)
+        for(var i = 0; i<this.letArr.length; i++)
         {
-                wordString += this.word[i] + " ";
+                wordString += this.letArr[i] + " ";
         }
         console.log(wordString);
     }
     // A function that takes a character as an argument and calls the 
     // guess function on each letter object (the second function 
     // defined in Letter.js)
-    this.checkGuessed = function(char) {
+    this.guessed = function(char) {
         var correct = false;
-        for (var i = 0; this.word.length; i++) {
-            if(this.word[i] === char) {
+        for (var i = 0; this.letArr.length; i++) {
+            if(letArr[i].isMatch(char)) {
                 correct = true;
                 this.numbersCorrect++;
-            } else {
-                break;
             }
             console.log(this.numbersCorrect);
             console.log(correct);
         }
-        return correct;
     }
 
-};
+}
 
-//Testing testing 123
-var word = new Word(input);
-word.showLetters();
-word.checkGuessed("A");
-word.showLetters();
-//console.log(word.checkGuessed("A"));
+//var word = new Word(input);
+//word.showLetters();
 //word.guessed("a");
 // if (A.isMatch("A")
 // console.log(A+" ");
+module.exports = Word;
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-var Word = require('./Word.js');
-var prompt = require("prompt");
+// GLOBAL VARIABLES 
+// =========================================================================
 
-//Randomly selects a word and uses Word constructor to store it
-var words = ["hola", "gustar", "caliente", "fuego", "fiesta", "cerveza", "caramba",
-"bonito", "querer"];
+// REQUIRE WORD FOLDER AND PROMPT
+var Word = require('./Word.js');
+var prompt = require('prompt');
+
+// // Solution will be held here
+// var chosenWord = "";
+// // This will break the solution into individual letters to be stored
+// var lettersInChosenWord = [];
+// // This will be the number of blanks we show based on the solution
+// var numBlanks = 0;
+// // Holds a mix of blank and solved letters
+// var newGame = new Word(chosenWord);
+
+// // Holds all of the wrong guesses
+// var wrongGuesses = [];
+
+// // Game counters
+// var winCounter = 0;
+// var lossCounter = 0;
+// var numGuesses = 10;
+
+
 
 //Prompts the user to guess and keeps track of the user's remaining guesses
 
@@ -14,5 +33,35 @@ console.log("Welcome to the hangman game you can play in terminal!" +
 "\nYou only have 10 guesses to get it right so chose wisely!" + 
 "\nReady to get started? (Y/N)");
 
+// FUNCTIONS
+// =========================================================================
+game = {
+    //Randomly selects a word and uses Word constructor to store it
+    words: ["hola", "gustar", "caliente", "fuego", "fiesta", "cerveza", "caramba",
+    "bonito", "querer"],
+    winCounter: 0,
+    numGuesses: 0,
+    lossCounter: 0,
+    wrongGuesses: [],
 
-wordInPlay = words[Math.floor(Math.random()* words.length)];
+    startGame: function(word) {
+        this.numGuesses = 10;
+        this.wordInPlay = new Word(this.words[Math.floor(Math.random() * this.words.length)]);
+        this.blanksAndSuccess = new Word(showLetters(this.wordInPlay));
+        this.wrongGuesses = [];
+        this.promptUser();
+;
+    }
+}
+
+
+// How we start the game
+function startGame() {
+    // Debugging
+    console.log("*************");
+
+}
+
+// MAIN PROCESS 
+// =========================================================================
+startGame();
