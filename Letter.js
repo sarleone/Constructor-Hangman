b4e5baf03b594adafff00b96fb2485d8f729f8d5
sarleone <sarah.m.leone@gmail.com>
2018-02-23 15:38:34 -0600

    updating documentation

--- Letter.js
+++ Letter.js
@@ -1,20 +1,16 @@
 // Letter.js: Contains a constructor, Letter. This constructor should be able
 // to either display an underlying character or a blank placeholder (such as 
 // an underscore), depending on whether or not the user has guessed the letter. 
-// That means the constructor should define:
 
-// A string value to store the underlying character for the letter
-// A boolean value that stores whether that letter has been guessed yet
-// A function that returns the underlying character if the letter has been 
-// guessed, or a placeholder (like an underscore) if the letter has not been 
-// guessed
-// A function that takes a character as an argument and checks it against the 
-// underlying character, updating the stored boolean value to true if it was 
-// guessed correctly
 
+// CONSTRUCTOR FUNCTION
+// =========================================================================
 function Letter(char) {
+    // A string value to store the underlying character for the letter
     this.char = char;
+    // A boolean value that stores whether that letter has been guessed yet
     this.guessed = false;
+    // A function that returns the either the word guessed or '_'
     this.toString = function() {
         if (this.guessed)
         {
@@ -22,6 +18,7 @@ function Letter(char) {
         }
         return "_"; 
     }
+    // A function that takes letter and updates boolean value to true if guessed correctly
     this.isMatch = function(letter) {
         if (this.char === letter) {
             this.guessed = true;
@@ -30,7 +27,7 @@ function Letter(char) {
         return false;
     }
 }
-
+// Export this fuction for Word.js
 module.exports = Letter;
 
 //to test
